19d1fe7882ac07c84f34e81fdb321bcc64eb3a2e
Jair Júnior <Jair Jnior>
2016-10-08 21:37:16 -0300

    alteraçao em arquivo q

--- app/Http/Controllers/Admin/VinculotabelasController.php
+++ app/Http/Controllers/Admin/VinculotabelasController.php
@@ -6,9 +6,12 @@ use App\Http\Requests;
 use App\Http\Controllers\Controller;
 
 use App\Vinculotabela;
+use App\Tabela;
+use App\Parametro;
 use App\tabelascoluna;
 use Illuminate\Http\Request;
 use Carbon\Carbon;
+use PDO;
 use Session;
 
 class VinculotabelasController extends Controller
@@ -47,11 +50,28 @@ class VinculotabelasController extends Controller
     public function store(Request $request)
     {
         $this->validate($request, ['id_tabela1' => 'required', 'id_tabela2' => 'required', ]);
-        $tabela1=$request->get('tabela1');
-        $tabela2=$request->get('tabela2');
+        $tabela1=$request->get('id_tabela1');
 
-        
+        $tabela2=$request->get('id_tabela2');
+        $table1 = Tabela::findOrFail($tabela1);
 
+        $id_parametro1=$table1->id_parametro;
+        $parametro1=Parametro::findOrFail($id_parametro1);
+
+
+
+            $conexao1=new PDO('mysql:host=' . $parametro1->host . ';dbname=' . $parametro1->banco, $parametro1->usuario, '');
+        $table2=Tabela::findOrFail($tabela2);
+        $parametro2=Parametro::findOrFail($table2->id_parametro);
+
+        $conexao2=new PDO('mysql:host=' . $parametro2->host . ';dbname=' . $parametro2->banco, $parametro2->usuario, '');
+
+        $sql1="alter table ".$table1->nome." add updated_at TIMESTAMP NOT NULL 
+        DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP";
+        $conexao1->query($sql1);
+        $sql2="alter table ".$table2->nome." add updated_at TIMESTAMP NOT NULL 
+        DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP";
+        $conexao2->query($sql2);
 
             Vinculotabela::create($request->all());
             Session::flash('flash_message', 'Vinculo tabela adicionado!');
